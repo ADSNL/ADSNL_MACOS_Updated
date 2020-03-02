904c4f2070689781b85c407d11c98436760f267c
unknown <echen2@pace.edu>
2020-03-02 17:25:21 -0500

    render data

--- client/src/App.js
+++ client/src/App.js
@@ -1,59 +1,52 @@
-import React, { Component } from 'react';
-import './App.css';
-import TopSellingBooks from './components/TopSelling/TopSellingBooks';
-import TopSellingClothing from './components/TopSelling/TopSellingClothing';
+import React, { Component } from "react";
+import "./App.css";
+import TopSellingBooks from "./components/TopSelling/TopSellingBooks";
+import TopSellingClothing from "./components/TopSelling/TopSellingClothing";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       renderedResponse: []
-    }
+    };
   }
 
   getResponse = async () => {
     fetch("http://localhost:5000/api/response")
-    .then((res) => {
-      return res.json();
-    })
-      .then((data) => {
-          this.setState({
-            renderedResponse : data
-          })
+      .then(res => {
+        return res.json();
       })
-     
+      .then(data => {
+        //let _DataArray = [];
+        this.setState({
+          renderedResponse: data
+        });
+      })
+
       .catch(err => err);
-      // console.log(this.state.renderedResponse);
-  }
+    // console.log(this.state.renderedResponse);
+  };
 
   componentDidMount() {
     this.getResponse();
   }
-  
-
 
   render() {
-const datas = this.state.renderedResponse
-   const datasList = datas.length ? (datas.map(data => {
-    return (
-<div className="App">
+    const datas = this.state.renderedResponse;
+    const datasList = datas.length ? (
+  
+        <div>
+          <TopSellingBooks  data={datas} />
+          
+       
+          {/* <TopSellingClothing /> */}
+        </div>
       
+    ) : (
+      <h1>Loading</h1>
+    );
 
-        
-        <TopSellingBooks key = {data.Book_ID} data={data}/>
-        {console.log(data.Book_ID)}
-        {/* <TopSellingClothing /> */}
-       
-      </div>
-    ) 
-  })) : (<h1>Loading</h1>)
-
-    return (
-<div>
-       {datasList}
-       
-       </div> 
-    ) 
+    return <div>{datasList}</div>;
   }
 }
 
---
+++ client/src/components/Cards.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+//import './Footer.css';
+import {
+  Card,
+  Button,
+  CardImg,
+  CardTitle,
+  CardSubtitle,
+  CardBody
+} from "reactstrap";
+
+class Cards extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      myData: props.data
+    };
+  }
+
+  render() {
+    const datas = this.state.myData;
+    const datasList = datas.length ? ( datas.map( item  => {
+      return(
+
+             <Card>
+      <CardImg
+        top
+        width="50"
+        height="200"
+        src="https://dictionary.cambridge.org/us/images/thumb/book_noun_001_01679.jpg?version=5.0.70"
+        alt="Card image cap"
+      />
+      <CardBody>
+        <CardTitle>
+          <b>{item.Book_Title}</b>
+          {console.log(item.Book_Title)}
+          
+        </CardTitle>
+        <CardSubtitle>{item.ISBN_10}</CardSubtitle>
+  
+        <Button>Add to Cart</Button>
+      </CardBody>
+    </Card>
+        )
+    })
+
+    ) : (
+      <h1>Loading</h1>
+    );
+    {console.log(datasList)}
+  
+    return <div>
+      {datasList}
+    </div>
+  }
+ 
+
+}
+
+export default Cards;
