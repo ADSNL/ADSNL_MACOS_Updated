f2a1c26501fd0a7432c975f7d0e9f7304ef699a8
NaikMV <60404232+NaikMV@users.noreply.github.com>
2020-03-23 16:55:01 -0400

    Loaded Book data in TileView

--- client/src/components/TileRow/TileRow.js
+++ client/src/components/TileRow/TileRow.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {
   Button, Modal, ModalHeader, ModalBody, ModalFooter, Media, Card, CardImg, CardTitle, CardDeck,
-  CardSubtitle, CardBody, Breadcrumb, BreadcrumbItem
+  CardSubtitle, CardBody, Breadcrumb, BreadcrumbItem, Col, Row, Box
 } from 'reactstrap';
 import ProductModal from "../ProductModal";
 
@@ -15,7 +15,7 @@ class TileRow extends Component {
   }
 
   getBooks = async () => {
-    fetch("http://localhost:5000/api/books")
+    fetch("http://localhost:5000/api/viewBooks")
       .then(res => {
         return res.json();
       })
@@ -35,18 +35,19 @@ class TileRow extends Component {
 
     let tiles = []
     const datas = this.state.books;
-
+    
     for (let j = 0; j < datas.length; j++) {
       tiles.push(
-        <Card>
-          <CardImg top width="50" height="200" src="https://dictionary.cambridge.org/us/images/thumb/book_noun_001_01679.jpg?version=5.0.70" alt="Card image cap" />
-          <CardBody>
-            <CardTitle>{datas[j].Book_Title}</CardTitle>
-            <CardSubtitle> $ {datas[j].Unit_Price}</CardSubtitle>
-            <ProductModal buttonLabel="View Details" />
-            {/* <ProductModal buttonLabel="Add to Cart" modalTitle={props.data[0].Book_Title} modalText={props.data[0].ISBN_10} modalImg="https://dictionary.cambridge.org/us/images/thumb/book_noun_001_01679.jpg?version=5.0.70"/> */}
-          </CardBody>
-        </Card>
+        <Col md="3">
+          <Card>
+            <CardImg top width="50" height="200" src="https://dictionary.cambridge.org/us/images/thumb/book_noun_001_01679.jpg?version=5.0.70" alt="Card image cap" />
+            <CardBody>
+              <CardTitle>{datas[j].Book_Title}</CardTitle>
+              <CardSubtitle> $ {datas[j].Unit_Price}</CardSubtitle>
+              <ProductModal buttonLabel="View Details" modalTitle={datas[j].Book_Title} modalText={datas[j].ISBN_10} modalImg="https://dictionary.cambridge.org/us/images/thumb/book_noun_001_01679.jpg?version=5.0.70" />
+            </CardBody>
+          </Card>
+        </Col>
       )
     }
 
--- client/src/components/TileView/TileView.js
+++ client/src/components/TileView/TileView.js
@@ -1,61 +1,70 @@
 import React, { useState } from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter,Media, Card, CardImg, CardTitle, CardDeck,
-    CardSubtitle, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import {
+  Button, Modal, ModalHeader, ModalBody, ModalFooter, Media, Card, CardImg, CardTitle, CardDeck,
+  CardSubtitle, CardBody, Breadcrumb, BreadcrumbItem, Row, Container
+} from 'reactstrap';
 import ProductModal from "../ProductModal";
 // import { ModalTitle } from 'react-bootstrap';
 import TileRow from "../TileRow/TileRow";
 
 const TileView = (props) => {
   const {
-   productName=[],
-   productPrice=[],
-   productImg=[]
+    productName = [],
+    productPrice = [],
+    productImg = []
   } = props;
 
   let tiles = []
   tiles.push(
-    
+
     <Breadcrumb tag="nav" listTag="div">
-    <BreadcrumbItem className="container" tag="a" href="#">
-      <b>Books</b>
-    </BreadcrumbItem>
+      <BreadcrumbItem className="container" tag="a" href="#">
+        <b>Books</b>
+      </BreadcrumbItem>
     </Breadcrumb>
   )
-//   for(let i =0; i <5; i+5){
-//       let elements =[]
-//       for (let j=i; j<i+5; j++){
-//         elements.push(
-//         <Card>
-//             <CardImg top width="50" height="200" src="https://dictionary.cambridge.org/us/images/thumb/book_noun_001_01679.jpg?version=5.0.70" alt="Card image cap" /> 
-//               <CardBody>
-//                 <CardTitle>{productName[j]}</CardTitle>
-//                 <CardSubtitle> $ {productPrice[j]}</CardSubtitle>
-//                 {/* <ProductModal buttonLabel="Add to Cart" modalTitle={props.data[0].Book_Title} modalText={props.data[0].ISBN_10} modalImg="https://dictionary.cambridge.org/us/images/thumb/book_noun_001_01679.jpg?version=5.0.70"/> */}
-//               </CardBody>
-//         </Card>
-//         )
-//       }
-// for (let i=0; i<2; i++){
-//     tiles.push(<CardDeck>)
-    for (let j=0; j<5; j++){
-      tiles.push(
-      // <Card>
-      //     <CardImg top width="50" height="200" src="https://dictionary.cambridge.org/us/images/thumb/book_noun_001_01679.jpg?version=5.0.70" alt="Card image cap" /> 
-      //       <CardBody>
-      //         <CardTitle>{productName[j]}</CardTitle>
-      //         <CardSubtitle> $ {productPrice[j]}</CardSubtitle>
-      //         {/* <ProductModal buttonLabel="Add to Cart" modalTitle={props.data[0].Book_Title} modalText={props.data[0].ISBN_10} modalImg="https://dictionary.cambridge.org/us/images/thumb/book_noun_001_01679.jpg?version=5.0.70"/> */}
-      //       </CardBody>
-      // </Card>
-        <CardDeck>
-          <TileRow />
-        </CardDeck>
-      )
-    
-      {/* tiles.push(</CardDeck>) */}
-    //   tiles.push({elements})
+  tiles.push(
+    <Container>
+      <Row>
+        <TileRow />
+      </Row>
+    </Container>
+  );
+  //   for(let i =0; i <5; i+5){
+  //       let elements =[]
+  //       for (let j=i; j<i+5; j++){
+  //         elements.push(
+  //         <Card>
+  //             <CardImg top width="50" height="200" src="https://dictionary.cambridge.org/us/images/thumb/book_noun_001_01679.jpg?version=5.0.70" alt="Card image cap" /> 
+  //               <CardBody>
+  //                 <CardTitle>{productName[j]}</CardTitle>
+  //                 <CardSubtitle> $ {productPrice[j]}</CardSubtitle>
+  //                 {/* <ProductModal buttonLabel="Add to Cart" modalTitle={props.data[0].Book_Title} modalText={props.data[0].ISBN_10} modalImg="https://dictionary.cambridge.org/us/images/thumb/book_noun_001_01679.jpg?version=5.0.70"/> */}
+  //               </CardBody>
+  //         </Card>
+  //         )
+  //       }
+  // for (let i=0; i<2; i++){
+  //     tiles.push(<CardDeck>)
+  //   for (let j=0; j<5; j++){
+  //     tiles.push(
+  //     // <Card>
+  //     //     <CardImg top width="50" height="200" src="https://dictionary.cambridge.org/us/images/thumb/book_noun_001_01679.jpg?version=5.0.70" alt="Card image cap" /> 
+  //     //       <CardBody>
+  //     //         <CardTitle>{productName[j]}</CardTitle>
+  //     //         <CardSubtitle> $ {productPrice[j]}</CardSubtitle>
+  //     //         {/* <ProductModal buttonLabel="Add to Cart" modalTitle={props.data[0].Book_Title} modalText={props.data[0].ISBN_10} modalImg="https://dictionary.cambridge.org/us/images/thumb/book_noun_001_01679.jpg?version=5.0.70"/> */}
+  //     //       </CardBody>
+  //     // </Card>
+  //       <CardDeck>
+  //         <TileRow />
+  //       </CardDeck>
+  //     )
 
-  }
+  //     {/* tiles.push(</CardDeck>) */}
+  //   //   tiles.push({elements})
+
+  // }
 
   return tiles
 }
