d1707d28e4a2e8557df725e642be5b26000a7256
NaikMV <60404232+NaikMV@users.noreply.github.com>
2020-03-23 14:15:41 -0400

    Removed static data and passed data into TileRow

--- client/src/components/TileRow/TileRow.js
+++ client/src/components/TileRow/TileRow.js
@@ -1,53 +1,57 @@
-import React, { useState } from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter,Media, Card, CardImg, CardTitle, CardDeck,
-    CardSubtitle, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component } from 'react';
+import {
+  Button, Modal, ModalHeader, ModalBody, ModalFooter, Media, Card, CardImg, CardTitle, CardDeck,
+  CardSubtitle, CardBody, Breadcrumb, BreadcrumbItem
+} from 'reactstrap';
 import ProductModal from "../ProductModal";
-// import { ModalTitle } from 'react-bootstrap';
 
-const TileRow = (props) => {
-  const {
-   productName=[],
-   productPrice=[],
-   productImg=[]
-  } = props;
+class TileRow extends Component {
 
-  let tiles = []
-  
-//   for(let i =0; i <5; i+5){
-//       let elements =[]
-//       for (let j=i; j<i+5; j++){
-//         elements.push(
-//         <Card>
-//             <CardImg top width="50" height="200" src="https://dictionary.cambridge.org/us/images/thumb/book_noun_001_01679.jpg?version=5.0.70" alt="Card image cap" /> 
-//               <CardBody>
-//                 <CardTitle>{productName[j]}</CardTitle>
-//                 <CardSubtitle> $ {productPrice[j]}</CardSubtitle>
-//                 {/* <ProductModal buttonLabel="Add to Cart" modalTitle={props.data[0].Book_Title} modalText={props.data[0].ISBN_10} modalImg="https://dictionary.cambridge.org/us/images/thumb/book_noun_001_01679.jpg?version=5.0.70"/> */}
-//               </CardBody>
-//         </Card>
-//         )
-//       }
-// for (let i=0; i<2; i++){
-//     tiles.push(<CardDeck>)
-    for (let j=0; j<5; j++){
-      tiles.push(
-      <Card>
-          <CardImg top width="50" height="200" src="https://dictionary.cambridge.org/us/images/thumb/book_noun_001_01679.jpg?version=5.0.70" alt="Card image cap" /> 
-            <CardBody>
-              <CardTitle>{productName[j]}</CardTitle>
-              <CardSubtitle> $ {productPrice[j]}</CardSubtitle>
-              <ProductModal buttonLabel="Add to Cart"/>
-              {/* <ProductModal buttonLabel="Add to Cart" modalTitle={props.data[0].Book_Title} modalText={props.data[0].ISBN_10} modalImg="https://dictionary.cambridge.org/us/images/thumb/book_noun_001_01679.jpg?version=5.0.70"/> */}
-            </CardBody>
-      </Card>
-      )
-    
-      {/* tiles.push(</CardDeck>) */}
-    //   tiles.push({elements})
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: []
+    };
   }
 
-  return tiles
+  getBooks = async () => {
+    fetch("http://localhost:5000/api/books")
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        this.setState({
+          books: data
+        });
+      })
+      .catch(err => err);
+  };
+
+  componentDidMount() {
+    this.getBooks();
+  }
+
+  render() {
+
+    let tiles = []
+    const datas = this.state.books;
+
+    for (let j = 0; j < datas.length; j++) {
+      tiles.push(
+        <Card>
+          <CardImg top width="50" height="200" src="https://dictionary.cambridge.org/us/images/thumb/book_noun_001_01679.jpg?version=5.0.70" alt="Card image cap" />
+          <CardBody>
+            <CardTitle>{datas[j].Book_Title}</CardTitle>
+            <CardSubtitle> $ {datas[j].Unit_Price}</CardSubtitle>
+            <ProductModal buttonLabel="View Details" />
+            {/* <ProductModal buttonLabel="Add to Cart" modalTitle={props.data[0].Book_Title} modalText={props.data[0].ISBN_10} modalImg="https://dictionary.cambridge.org/us/images/thumb/book_noun_001_01679.jpg?version=5.0.70"/> */}
+          </CardBody>
+        </Card>
+      )
+    }
+
+    return tiles
+  }
 }
 
 export default TileRow;
