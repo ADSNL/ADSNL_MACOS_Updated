fb3f46ebde9b1a91203f04d5506a6466f02e17db
Prathmesh Pathak <34978373+prarthamjp786@users.noreply.github.com>
2020-04-06 17:19:47 -0400

    updated

--- client/src/components/SearchResults.js
+++ client/src/components/SearchResults.js
@@ -2,7 +2,6 @@ import React from 'react'
 import {
     Card, CardImg, CardTitle, CardSubtitle, CardBody, Col
 } from 'reactstrap';
-import ProductModal from "../ProductModal";
 
 const SearchResults = (props) => {
     return (
