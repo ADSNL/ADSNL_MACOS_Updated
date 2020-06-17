979422b7f0218d05c3d8d20309a2c6328ddaec32
Prathmesh Pathak <34978373+prarthamjp786@users.noreply.github.com>
2020-06-16 22:07:49 -0400

    backend Login route with JWT tokens created

--- routes/Users.js
+++ routes/Users.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const users = express.Router();
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const express = require('express')
+const users = express.Router()
+const cors = require('cors')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 
 const User = require('../models/User');
 users.use(cors());
@@ -45,5 +45,28 @@ users.post('/register', (req, res) => {
             res.send('Catch error: ' + err)
         })
 });
+
+users.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    })
+        .then(user => {
+            if (user) {
+                if (bcrypt.compareSync(req.body.password, user.password)) {
+                    let token = jwt.sign(user.dataValues, process.env.SECRET_KEY, {
+                        expiresIn: 1440
+                    })
+                    res.send(token)
+                }
+            } else {
+                res.status(400).json({ error: 'User does not exist' })
+            }
+        })
+        .catch(err => {
+            res.status(400).json({ error: err })
+        })
+})
 
 module.exports = users
