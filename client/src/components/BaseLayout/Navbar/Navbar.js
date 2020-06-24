ad49f4950914973e90aed75972a9b73ed85e0f11
Prathmesh Pathak <34978373+prarthamjp786@users.noreply.github.com>
2020-06-24 12:37:08 -0400

    Final changes

--- client/src/components/BaseLayout/Navbar/Navbar.js
+++ client/src/components/BaseLayout/Navbar/Navbar.js
@@ -17,8 +17,8 @@ import {
     Switch,
     Route,
     Link
-  } from "react-router-dom";
-import {CardDeck} from 'reactstrap'; //0305 test remove later
+} from "react-router-dom";
+import { CardDeck } from 'reactstrap'; //0305 test remove later
 
 import TileView from '../../TileView/TileView';
 
@@ -27,67 +27,67 @@ import TopSellingBooks from "../../TopSelling/TopSellingBooks";
 
 const Navbar = (props) => {
 
-    
+
     const [isOpen, setIsOpen] = useState(false);
 
     const toggle = () => setIsOpen(!isOpen);
 
     return (
-    <Router>
-    <div>
-        <NavbarB color="light"
-            light expand="md">
-            <NavbarBrand href="/"><Link to="/"><b>ADSNL</b></Link ></NavbarBrand>
-            <NavbarToggler onClick={toggle} />
-            <Collapse isOpen={isOpen} navbar>
-                <Nav color="light" className="mr-auto" navbar>
-                    <NavItem>
-                        <NavLink href="/components/"><Link to="/books"><b>Books</b></Link ></NavLink >
-                    </NavItem>
-                    <NavItem>
-                       
-                            
-                        <NavLink href="/components/"><b>Clothing</b></NavLink >
-                        
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="/components/"><b>Movies</b></NavLink >
-                    </NavItem><NavItem>
-                        <NavLink href="/components/"><b>Books</b></NavLink >
-                    </NavItem><NavItem>
-                        <NavLink href="/components/"><b>Kitchen</b></NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="/components/"> <b> Make up </b></NavLink >
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="/components/"><b>Pets</b></NavLink >
-                    </NavItem></Nav> <NavbarText><b>Login</b></NavbarText></Collapse>
-        </NavbarB>
-    </div>
-    {/* <Switch>
+        <Router>
+            <div>
+                <NavbarB color="light"
+                    light expand="md">
+                    <NavbarBrand href="/home"><Link to="/home"><b>ADSNL</b></Link ></NavbarBrand>
+                    <NavbarToggler onClick={toggle} />
+                    <Collapse isOpen={isOpen} navbar>
+                        <Nav color="light" className="mr-auto" navbar>
+                            <NavItem>
+                                <NavLink href="/components/"><Link to="/books"><b>Books</b></Link ></NavLink >
+                            </NavItem>
+                            <NavItem>
+
+
+                                <NavLink href="/components/"><b>Clothing</b></NavLink >
+
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="/components/"><b>Movies</b></NavLink >
+                            </NavItem><NavItem>
+                                <NavLink href="/components/"><b>Books</b></NavLink >
+                            </NavItem><NavItem>
+                                <NavLink href="/components/"><b>Kitchen</b></NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="/components/"> <b> Make up </b></NavLink >
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="/components/"><b>Pets</b></NavLink >
+                            </NavItem></Nav> <NavbarText><b>Login</b></NavbarText></Collapse>
+                </NavbarB>
+            </div>
+            {/* <Switch>
         <Route path="/books">
         
 
           <TileView/>
         
         </Route> */}
-        {/* <Route path="/">
+            {/* <Route path="/">
         
         <TopSellingBooks  data={props} />
         {console.log(props)}
         
       </Route> */}
-      {/* <Route exact path="/home"> */}
-       
+            {/* <Route exact path="/home"> */}
 
-       {/* <h1> hello</h1> */}
-       {/* <TopSellingClothing  /> */}
-       
-       {/* </Route> */}
 
-    {/* </Switch> */}
-    </Router>
+            {/* <h1> hello</h1> */}
+            {/* <TopSellingClothing  /> */}
+
+            {/* </Route> */}
+
+            {/* </Switch> */}
+        </Router>
     );
 }
 
