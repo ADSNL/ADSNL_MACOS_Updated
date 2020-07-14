8f63be2e0c5fcc68897d463f5db53957010dd1e5
Prathmesh Pathak <34978373+prarthamjp786@users.noreply.github.com>
2020-07-14 14:05:29 -0400

    Created CSS file for profile page

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button
+} from 'reactstrap';
+import './Profile.css';
 
 export default class Profile extends Component {
     render() {
         return (
             <div className="container">
-                <h1>Customer Profile...</h1>
+                <Card className="main-content">
+                    <CardImg top width="100%" src="/profile.png" alt="Card image cap" />
+                    <CardBody>
+                        <CardTitle>Card title</CardTitle>
+                        <CardSubtitle>Card subtitle</CardSubtitle>
+                        <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                    </CardBody>
+                </Card>
             </div>
         )
     }
