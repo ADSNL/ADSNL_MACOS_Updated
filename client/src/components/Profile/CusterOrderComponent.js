3c93ce47e12030f5cda6eb4a5e4f9347351fd84c
Prathmesh Pathak <34978373+prarthamjp786@users.noreply.github.com>
2020-08-24 16:05:26 -0400

    created function to return the order details for user

--- client/src/components/Profile/CusterOrderComponent.js
+++ client/src/components/Profile/CusterOrderComponent.js
@@ -1,6 +1,13 @@
 import React, { useState } from 'react';
 
 export default function CusterOrderComponent({ match }) {
+
+    const [customerOrderData] = useState(match.params.id);
+    getCustomerOrderData();
+    function getCustomerOrderData() {
+        console.log("getting customer order data." + customerOrderData);
+    }
+
     return (
         <div>
             <h3>ID : {match.params.id}</h3>
