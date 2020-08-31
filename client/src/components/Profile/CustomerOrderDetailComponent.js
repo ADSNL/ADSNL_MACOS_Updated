3045c47b14e3b14675796dc0bce892f90b895672
Prathmesh Pathak <34978373+prarthamjp786@users.noreply.github.com>
2020-08-31 14:05:31 -0400

    order detail component complete

--- client/src/components/Profile/CustomerOrderDetailComponent.js
+++ client/src/components/Profile/CustomerOrderDetailComponent.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Row, Col, Container } from 'reactstrap';
 
 export default class CustomerOrderDetailComponent extends Component {
     constructor(props) {
@@ -26,10 +27,43 @@ export default class CustomerOrderDetailComponent extends Component {
     };
 
     render() {
-        return (
-            <div>
+        let order = this.state.customerOrderData;
+        const order_tiles = [];
+        const order_heading = [];
 
-            </div>
+        order_heading.push(
+            <Row>
+                <Col>Customer ID</Col>
+                <Col>First Name</Col>
+                <Col>Last Name</Col>
+                <Col>City</Col>
+                <Col>Order ID</Col>
+                <Col>Order Date</Col>
+                <Col>Time</Col>
+            </Row>
+        );
+
+        for (let i = 0; i < order.length; i++) {
+            order_tiles.push(
+                <Row key={i}>
+                    <Col>{order[i].ID}</Col>
+                    <Col>{order[i].First_Name}</Col>
+                    <Col>{order[i].Last_Name}</Col>
+                    <Col>{order[i].City}</Col>
+                    <Col>{order[i].Order_ID}</Col>
+                    <Col>{order[i].Order_Date}</Col>
+                    <Col>{order[i].Time}</Col>
+                </Row>
+            );
+        }
+
+        return (
+            <Container className="themed-container" fluid={true}>
+                {order_heading}
+                {
+                    (order_tiles.length === 0 || order_tiles == undefined ? (<h2>Loading...</h2>) : order_tiles)
+                }
+            </Container>
         )
     }
 }
