82de980b0931d583c2ce56879897b59c5ff3bdfa
NaikMV <60404232+NaikMV@users.noreply.github.com>
2020-09-08 14:00:08 -0400

    Updated makeUp script

--- data/DataMigrationScripts/3.1_MakeUp.py
+++ data/DataMigrationScripts/3.1_MakeUp.py
@@ -15,4 +15,37 @@ conn_old = pyodbc.connect('Driver={SQL Server};'
 cursor_new = conn_new.cursor()
 cursor_old = conn_old.cursor()
 
-oldMakeUp = cursor_old.execute('SELECT M.Makeup_ID, M.Makeup_Category_ID, M.Makeup_Brand_ID, M.Makeup_Attribute_ID, M.Makeup_Name, M.Makeup_Volume, M.ASIN, MA.Makeup_Attribute_Name, MB.Makeup_Brand_Name, MC.Makeup_Category_Name FROM Makeup AS M JOIN Makeup_Attributes AS MA on M.Makeup_Attribute_ID = MA.Makeup_Attribute_ID JOIN Makeup_Brands AS MB on M.Makeup_Brand_ID = MB.Makeup_Brand_ID JOIN Makeup_Categories AS MC on M.Makeup_Category_ID = MC.Makeup_Category_ID')
+Color = cursor_old.execute('SELECT CL.Makeup_ID, C.Makeup_Color_Name, CL.Price FROM Makeup_Colors_Lookup AS CL JOIN Makeup_Colors AS C on CL.Makeup_Color_ID = C.Makeup_Color_ID')
+ColorCount = 0
+
+for row in Color:
+    Color_ID = 0
+
+    col = cursor_new.execute('SELECT Color_ID FROM Color Where Color_Name = ?', row[1])
+
+    for Colrow in col:
+        Color_ID = Colrow[0]
+
+    if(Color_ID == 0):
+        cursor_new.execute('Insert Into Color Values(?)', row[1])
+        col = cursor_new.execute('SELECT Color_ID FROM Color Where Color_Name = ?', row[1])
+        
+        for Colrow in col:
+            Color_ID = Colrow[0]
+
+    ID = str(row[0])
+    
+    if len(ID) == 1:
+        ID = "300000" + ID
+    elif len(ID) == 2:
+        ID = "30000" + ID
+    elif len(ID) == 3:
+        ID = "3000" + ID
+
+    ID = int(ID)
+
+    cursor_new.execute('Insert Into Feature_Lookup (Prod_SKU, Color_ID, Feature_Price) Values(?,?,?)', ID, Color_ID, row[2])
+    ColorCount = ColorCount + 1
+
+print(str(ColorCount) + ' colors interted successfully!')
+conn_new.commit()
