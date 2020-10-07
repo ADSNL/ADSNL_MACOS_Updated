88baf5c3e7d82dbf361e873c0f23b1d05300556e
Prathmesh Pathak <34978373+prarthamjp786@users.noreply.github.com>
2020-10-07 15:48:20 -0400

    Formatted query

--- server.js
+++ server.js
@@ -35,7 +35,9 @@ app.get('/api/books', (req, res) => {
       console.log(err);
       return;
     }
-    req.query('SELECT TOP 4 Books.Book_ID,Books.Book_Title as Title,Books.ISBN_10 as Number,ISBN_13,Book_Genre_ID,Book_Publisher_ID,min(Unit_Price) as Price FROM  Books JOIN dbo.Book_Media_Lookup ON Books.Book_ID=Book_Media_Lookup.Book_ID GROUP BY Books.Book_ID,Book_Title,ISBN_10,ISBN_13,Book_Genre_ID,Book_Publisher_ID    ', (err, recordset) => {
+    req.query(`SELECT TOP 4 Books.Book_ID,Books.Book_Title as Title,Books.ISBN_10 as Number,ISBN_13,Book_Genre_ID,Book_Publisher_ID,min(Unit_Price) as Price 
+                  FROM  Books JOIN dbo.Book_Media_Lookup ON Books.Book_ID=Book_Media_Lookup.Book_ID 
+                  GROUP BY Books.Book_ID,Book_Title,ISBN_10,ISBN_13,Book_Genre_ID,Book_Publisher_ID`, (err, recordset) => {
       if (err) {
         console.log(err);
         return;
@@ -170,8 +172,9 @@ app.get('/api/customer', (req, res) => {
 
     else {
       req.query(`select top 10 Customer_ID as ID, Customer_FName as FName, Customer_LName as LName, 
-    Sex as Gender, Birth_Date as DOB, Zip_Code as Zip, City, State, Street_Name as StreetName, 
-    Street_Number as Street, Street_Type as Avenue from Customer_Master where Customer_FName like '%` + first_name + `%' and Customer_LName like '%` + last_name + `%'`, (err, recordset) => {
+                  Sex as Gender, Birth_Date as DOB, Zip_Code as Zip, City, State, Street_Name as StreetName, 
+                  Street_Number as Street, Street_Type as Avenue from Customer_Master 
+                  where Customer_FName like '%` + first_name + `%' and Customer_LName like '%` + last_name + `%'`, (err, recordset) => {
         if (err) {
           console.log(err);
           return;
@@ -201,8 +204,10 @@ app.get('/api/viewBooks', (req, res) => {
     req.query(`SELECT BD.Book_ID, BD.Title, BD.Number, BD.ISBN_13, BD.Book_Genre_ID, BD.Book_Publisher_ID, BD.Price,
     (SELECT COUNT(DISTINCT Books.Book_ID) FROM Books JOIN dbo.Book_Media_Lookup ON Books.Book_ID = Book_Media_Lookup.Book_ID) AS CatCount
     FROM (
-        SELECT Books.Book_ID,Book_Title as Title,ISBN_10 as Number,ISBN_13,Book_Genre_ID,Book_Publisher_ID,min(Unit_Price) as Price, ROW_NUMBER() OVER (ORDER BY Books.Book_ID) AS RowNum
-        FROM  Books JOIN dbo.Book_Media_Lookup ON Books.Book_ID=Book_Media_Lookup.Book_ID GROUP BY Books.Book_ID,Book_Title,ISBN_10,ISBN_13,Book_Genre_ID,Book_Publisher_ID
+        SELECT Books.Book_ID,Book_Title as Title,ISBN_10 as Number,ISBN_13,Book_Genre_ID,Book_Publisher_ID,min(Unit_Price) as Price, 
+        ROW_NUMBER() OVER (ORDER BY Books.Book_ID) AS RowNum
+        FROM  Books JOIN dbo.Book_Media_Lookup ON Books.Book_ID=Book_Media_Lookup.Book_ID GROUP BY Books.Book_ID,
+        Book_Title,ISBN_10,ISBN_13,Book_Genre_ID,Book_Publisher_ID
     ) AS BD
     WHERE BD.RowNum BETWEEN `+ startRange + `and ` + endRange, (err, recordset) => {
       if (err) {
@@ -231,7 +236,8 @@ app.get('/api/viewClothing', (req, res) => {
     }
     req.query(`SELECT C.Number, C.Title, C.Price, C.CatCount
     FROM (
-        SELECT Clothing_ID as Number, Clothing_Name as Title, Price, (SElECT Count(*) FROM Clothing) AS CatCount, ROW_NUMBER() OVER (ORDER BY Clothing.Clothing_ID) AS RowNum
+        SELECT Clothing_ID as Number, Clothing_Name as Title, Price, (SElECT Count(*) FROM Clothing) AS CatCount, 
+        ROW_NUMBER() OVER (ORDER BY Clothing.Clothing_ID) AS RowNum
         FROM Clothing
     ) AS C
     WHERE C.RowNum BETWEEN `+ startRange + `and ` + endRange, (err, recordset) => {
@@ -293,7 +299,9 @@ app.get('/api/viewKitchen', (req, res) => {
     }
     req.query(`SELECT K.Number, K.Title, K.Price, K.CatCount
     FROM (
-        SELECT K.Kitchen_Product_ID as Number, K.Kitchen_Product_Name as Title, KT.Price as Price, (SElECT Count(DISTINCT K.Kitchen_Product_ID) FROM Kitchen AS K JOIN Kitchen_Types_Lookup AS KT ON K.Kitchen_Product_ID = KT.Kitchen_Product_ID) AS CatCount, ROW_NUMBER() OVER (ORDER BY k.Kitchen_Product_ID) AS RowNum
+        SELECT K.Kitchen_Product_ID as Number, K.Kitchen_Product_Name as Title, KT.Price as Price, (SElECT Count(DISTINCT K.Kitchen_Product_ID) 
+        FROM Kitchen AS K JOIN Kitchen_Types_Lookup AS KT ON K.Kitchen_Product_ID = KT.Kitchen_Product_ID) AS CatCount, 
+        ROW_NUMBER() OVER (ORDER BY k.Kitchen_Product_ID) AS RowNum
         FROM Kitchen AS K JOIN Kitchen_Types_Lookup AS KT ON K.Kitchen_Product_ID = KT.Kitchen_Product_ID
     ) AS K
     WHERE K.RowNum BETWEEN `+ startRange + `and ` + endRange, (err, recordset) => {
@@ -386,8 +394,10 @@ app.get('/api/search/', (req, res) => {
       req.query(`SELECT BD.Book_ID, BD.Title, BD.Number, BD.ISBN_13, BD.Book_Genre_ID, BD.Book_Publisher_ID, BD.Price,
     (SELECT COUNT(DISTINCT Books.Book_ID) FROM Books JOIN dbo.Book_Media_Lookup ON Books.Book_ID = Book_Media_Lookup.Book_ID) AS CatCount
     FROM (
-        SELECT Books.Book_ID,Book_Title as Title,ISBN_10 as Number,ISBN_13,Book_Genre_ID,Book_Publisher_ID,min(Unit_Price) as Price, ROW_NUMBER() OVER (ORDER BY Books.Book_ID) AS RowNum
-        FROM  Books JOIN dbo.Book_Media_Lookup ON Books.Book_ID=Book_Media_Lookup.Book_ID GROUP BY Books.Book_ID,Book_Title,ISBN_10,ISBN_13,Book_Genre_ID,Book_Publisher_ID
+        SELECT Books.Book_ID,Book_Title as Title,ISBN_10 as Number,ISBN_13,Book_Genre_ID,Book_Publisher_ID,min(Unit_Price) as Price, 
+        ROW_NUMBER() OVER (ORDER BY Books.Book_ID) AS RowNum
+        FROM  Books JOIN dbo.Book_Media_Lookup ON Books.Book_ID=Book_Media_Lookup.Book_ID GROUP BY Books.Book_ID,Book_Title,
+        ISBN_10,ISBN_13,Book_Genre_ID,Book_Publisher_ID
     ) AS BD
     WHERE BD.Title like '%` + parameter + `%'`, (err, recordset) => {
         if (err) {
@@ -404,7 +414,8 @@ app.get('/api/search/', (req, res) => {
     if (category == 'Clothing') {
       req.query(`SELECT C.Number, C.Title, C.Price, C.CatCount
       FROM (
-          SELECT Clothing_ID as Number, Clothing_Name as Title, Price, (SElECT Count(*) FROM Clothing) AS CatCount, ROW_NUMBER() OVER (ORDER BY Clothing.Clothing_ID) AS RowNum
+          SELECT Clothing_ID as Number, Clothing_Name as Title, Price, (SElECT Count(*) FROM Clothing) AS CatCount, 
+          ROW_NUMBER() OVER (ORDER BY Clothing.Clothing_ID) AS RowNum
           FROM Clothing
       ) AS C
     WHERE C.Title like '%` + parameter + `%'`, (err, recordset) => {
@@ -440,7 +451,9 @@ app.get('/api/search/', (req, res) => {
     if (category == "Kitchen") {
       req.query(`SELECT K.Number, K.Title, K.Price, K.CatCount
       FROM (
-          SELECT K.Kitchen_Product_ID as Number, K.Kitchen_Product_Name as Title, KT.Price as Price, (SElECT Count(DISTINCT K.Kitchen_Product_ID) FROM Kitchen AS K JOIN Kitchen_Types_Lookup AS KT ON K.Kitchen_Product_ID = KT.Kitchen_Product_ID) AS CatCount, ROW_NUMBER() OVER (ORDER BY k.Kitchen_Product_ID) AS RowNum
+          SELECT K.Kitchen_Product_ID as Number, K.Kitchen_Product_Name as Title, KT.Price as Price, 
+          (SElECT Count(DISTINCT K.Kitchen_Product_ID) FROM Kitchen AS K JOIN Kitchen_Types_Lookup AS KT ON K.Kitchen_Product_ID = KT.Kitchen_Product_ID) AS CatCount, 
+          ROW_NUMBER() OVER (ORDER BY k.Kitchen_Product_ID) AS RowNum
           FROM Kitchen AS K JOIN Kitchen_Types_Lookup AS KT ON K.Kitchen_Product_ID = KT.Kitchen_Product_ID
       ) AS K
     WHERE K.Title like '%` + parameter + `%'`, (err, recordset) => {
@@ -458,7 +471,8 @@ app.get('/api/search/', (req, res) => {
     if (category == "Make up") {
       req.query(`SELECT M.Number, M.Title, M.Price, M.CatCount
       FROM (
-          SELECT Makeup_ID as Number, Makeup_Name as Title, 29 as Price, (SElECT Count(*) FROM Makeup) AS CatCount, ROW_NUMBER() OVER (ORDER BY Makeup_ID) AS RowNum
+          SELECT Makeup_ID as Number, Makeup_Name as Title, 29 as Price, (SElECT Count(*) FROM Makeup) AS CatCount, 
+          ROW_NUMBER() OVER (ORDER BY Makeup_ID) AS RowNum
           FROM Makeup
       ) AS M
     WHERE M.Title like '%` + parameter + `%'`, (err, recordset) => {
