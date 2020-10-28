90419139d762c815fde2cc7d17577d71f1a95e08
Prathmesh Pathak <34978373+prarthamjp786@users.noreply.github.com>
2020-10-28 14:47:08 -0400

    New database queries

--- newSQlQueries.sql
+++ newSQlQueries.sql
@@ -1,4 +1,3 @@
-
 --query to get top 4 books data
 select top 4
     pi.Prod_SKU as SKU, pi.Dept_ID as deptID, pi.Prod_Name as Title, pi.Prod_ISBN_10 as Number, ml.Media_Price as Price
@@ -20,31 +19,62 @@ from Product_Info as pi
 where Dept_ID = 5;
 
 --query to get books for category page.
-select bd.Prod_SKU as SKU, bd.Title, bd.Number, bd.Prod_ISBN_13, bd.Price,
-    (select count(distinct Product_Info.Prod_SKU)
-    from Product_Info
-        join Media_Lookup
-        on Product_Info.Prod_SKU = Media_Lookup.Prod_SKU) as CatCount
+select BD.Number, BD.Title, BD.Price, BD.CatCount
 from (
-				select Product_Info.Prod_SKU, Product_Info.Prod_Name as Title, Product_Info.Prod_ISBN_10 as Number,
-        Product_Info.Prod_ISBN_13, min(Media_Price) as Price,
-        ROW_NUMBER() over (order by Product_Info.Prod_SKU) as RowNum
-    from Product_Info join dbo.Media_Lookup
-        on Product_Info.Prod_SKU = Media_Lookup.Prod_SKU
-    group by Product_Info.Prod_SKU, Product_Info.Prod_Name, Product_Info.Prod_ISBN_10, Product_Info.Prod_ISBN_13
-			) as bd
---WHERE BD.RowNum BETWEEN
+	select pi.Prod_SKU as Number, pi.Prod_Name as Title, ml.Media_Price as Price,
+	(select count(*) from Product_Info where Dept_ID = 1) as CatCount,
+	ROW_NUMBER() over (order by pi.Prod_SKU) as RowNum
+	from Product_Info as pi join Media_Lookup as ml
+	on pi.Prod_SKU = ml.Prod_SKU
+	where pi.Dept_ID = 1
+) as BD 
+--WHERE BD.RowNum BETWEEN `+ startRange + `and ` + endRange
 
 
 --query to get clothing for category page
 select C.Number, C.Title, C.Price, C.CatCount
-from (
-		select Prod_SKU as Number, Prod_Name as Title, Prod_Price as Price,
-        (select count(*)
-        from Product_Info
-        where Dept_ID = 2) as CatCount,
-        ROW_NUMBER() over (order by Product_Info.Prod_SKU) as RowNum
-    from Product_Info
-    where Dept_ID = 2
+	from (
+		select pi.Prod_SKU as Number, pi.Prod_Name as Title, pi.Prod_Price as Price,
+		(select count(*) from Product_Info where Dept_ID = 2) as CatCount,
+		ROW_NUMBER() over (order by pi.Prod_SKU) as RowNum
+		from Product_Info as pi
+		where pi.Dept_ID = 2
 	) as C
+	--WHERE C.RowNum BETWEEN `+ startRange + `and ` + endRange
 
+--query to get movies for category page
+select M.Number, M.Title, M.Price, M.CatCount
+	from (	select pi.Prod_SKU as Number, pi.Prod_Name as Title, fl.Feature_Price as Price,
+			(select count(*) from Product_Info where Dept_ID = 5) as CatCount, 
+			ROW_NUMBER() over (order by pi.Prod_SKU) as RowNum
+			from Product_Info as pi join Feature_Lookup as fl
+			on pi.Prod_SKU = fl.Prod_SKU 
+			where pi.Dept_ID = 5
+		) as M
+	--WHERE M.RowNum BETWEEN `+ startRange + `and ` + endRange
+
+
+--query to kitchen for category page
+select K.Number, K.Title, K.Price, K.CatCount
+from (
+		select pi.Prod_SKU as Number, pi.Prod_Name as Title, tl.Type_Price as Price,
+		(select count(*) from Product_Info where Dept_ID = 4) as CatCount,
+			ROW_NUMBER() over (order by pi.Prod_SKU) as RowNum
+			from Product_Info as pi join Type_Lookup as tl
+			on pi.Prod_SKU = tl.Prod_SKU
+			where pi.Dept_ID = 4
+	) as K
+	--WHERE K.RowNum BETWEEN `+ startRange + `and ` + endRange
+
+
+--query to get makeup for category page
+select M.Number, M.Title, M.Price, M.CatCount
+	from (
+		select pi.Prod_SKU as Number, pi.Prod_Name as Title, fl.Feature_Price as Price,
+		(select count(*) from Product_Info where Dept_ID = 3) as CatCount,
+		ROW_NUMBER() over (order by pi.Prod_SKU) as RowNum
+		from Product_Info as pi join Feature_Lookup as fl
+		on pi.Prod_SKU = fl.Prod_SKU
+		where pi.Dept_ID = 3
+	) as M
+	--WHERE M.RowNum BETWEEN `+ startRange + `and ` + endRange
